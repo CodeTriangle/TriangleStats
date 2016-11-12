94df751f3103b6d824b6371a0f19738c9389990e
Richard Snider <lighthousemaniac@gmail.com>
2016-11-12 16:19:46 -0700

    Experiment with Advanced Adaptive Graphing.

---
+++ js/adaptive-graphing-advanced.js
@@ -0,0 +1,37 @@
+var xmax, ymax, xmin, ymin, xunit, yunit, xcoord, ycoord;
+
+function update() {
+  updateWindow();
+  graphPoint();
+}  
+
+function updateWindow() {
+  xmax = parseFloat($("#xmax").prop("value"));
+  ymax = parseFloat($("#ymax").prop("value"));
+  xmin = parseFloat($("#xmin").prop("value"));
+  ymin = parseFloat($("#ymin").prop("value"));
+
+  xunit = 600/(xmax - xmin);
+  yunit = 500/(ymax - ymin);
+  
+  updateAxes();
+  updatePoint();
+}
+
+function updateAxes() {
+  var xpath = "M 0 " + (-ymin * yunit) + " L 600 " + (-ymin * yunit);
+  var ypath = "M " + (-xmin * yunit) + " 0 L " + (-xmin * yunit) + " 500";
+  $("#xaxis").attr("d", xpath);
+  $("#yaxis").attr("d", ypath);
+}
+
+function graphPoint() {
+  xcoord = parseFloat($("#xc").prop("value"));
+  ycoord = parseFloat($("#yc").prop("value"));
+  updatePoint();
+}
+
+function updatePoint() {
+  $("#changed").attr("cx", (xcoord - xmin) * xunit);
+  $("#changed").attr("cy", 500-((ycoord - ymin) * yunit));
+}
